36fc08a7bb8e272ff0638684fba0bca9562dcaea
faisaldp123 <faisal.dpathshala@gmail.com>
2026-04-07 16:30:36 +0530

    added render url for calling backend in api.js file

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://all-india-boards-admin-backend.onrender.com/api",
 });
 
 // Attach token automatically
